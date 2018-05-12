36eb0ddd077d03907a3b16bd9c67609ca86efc0c
anthonyDPZ <duprez.a@gmail.com>
2018-05-12 13:02:55 +0200

    fix
    fix

--- projetLua/PERSO_Gui/Full Demo/main.lua
+++ projetLua/PERSO_Gui/Full Demo/main.lua
@@ -9,7 +9,7 @@ love.graphics.setDefaultFilter("nearest")
 if arg[#arg] == "-debug" then require("mobdebug").start() end
 
 local myGUI = require("GCGUI")
-local mainFont = love.graphics.newFont("kenvector_future_thin.ttf", 15)
+local mainFont = love.graphics.newFont("Font/kenvector_future_thin.ttf", 15)
 love.graphics.setFont(mainFont)
 
 local groupTest
@@ -33,33 +33,33 @@ function love.load()
   
   buttonYes = myGUI.newButton(10, 20, 100, 43,"Yes", mainFont, {151, 220, 250})
   buttonYes:setImages(
-    love.graphics.newImage("button_default.png"),
-    love.graphics.newImage("button_hover.png"),
-    love.graphics.newImage("button_pressed.png")
+    love.graphics.newImage("images/Button/button_default.png"),
+    love.graphics.newImage("images/Button/button_hover.png"),
+    love.graphics.newImage("images/Button/button_pressed.png")
     )
   buttonNo = myGUI.newButton(115, 20, 100, 43,"No", mainFont, {151, 220, 250})
   buttonNo:setImages(
-    love.graphics.newImage("button_default.png"),
-    love.graphics.newImage("button_hover.png"),
-    love.graphics.newImage("button_pressed.png")
+    love.graphics.newImage("images/Button/button_default.png"),
+    love.graphics.newImage("images/Button/button_hover.png"),
+    love.graphics.newImage("images/Button/button_pressed.png")
     )
   
   buttonTest3 = myGUI.newButton(55, 100, 120, 80,"No images", mainFont, {250, 250, 250})
   
   panelTest1 = myGUI.newPanel(10, 220, 300, 200)
-  panelTest1:setImage(love.graphics.newImage("panel1.png"))
+  panelTest1:setImage(love.graphics.newImage("images/Panel/panel1.png"))
   panelTest1:setEvent("hover", onPanelHover)
 
   panelTest2 = myGUI.newPanel(350, 250)
-  panelTest2:setImage(love.graphics.newImage("panel2.png"))
+  panelTest2:setImage(love.graphics.newImage("images/Panel/panel2.png"))
 
   textTest = myGUI.newText(panelTest1.X+10, panelTest1.Y,
                            300, 28, "HULL STATUS", mainFont, "", "center", {151, 220, 250})
   
   checkBoxTest1 = myGUI.newCheckbox(250, 30, 24, 24)
   checkBoxTest1:setImages(
-    love.graphics.newImage("dotRed.png"),
-    love.graphics.newImage("dotGreen.png")
+    love.graphics.newImage("images/dotRed.png"),
+    love.graphics.newImage("images/dotGreen.png")
   )
   checkBoxTest1:setEvent("pressed", onCheckboxSwitch)
   
@@ -69,8 +69,8 @@ function love.load()
   progressTest1 = myGUI.newProgressBar(panelTest1.X + 35, panelTest1.Y + 68, 220, 26, 100,
                                       {50,50,50}, {250, 129, 50})
                                     
-  progressTest1:setImages(love.graphics.newImage("progress_grey.png"),
-                         love.graphics.newImage("progress_orange.png"))
+  progressTest1:setImages(love.graphics.newImage("images/progress_grey.png"),
+                         love.graphics.newImage("images/progress_orange.png"))
 
   
   title2 = myGUI.newText(panelTest1.X + 35, panelTest1.Y + 45 + 70, 0, 0,
@@ -79,8 +79,8 @@ function love.load()
   progressTest2 = myGUI.newProgressBar(panelTest1.X + 35, panelTest1.Y + 68 + 70, 220, 26, 100,
                                       {50,50,50}, {250, 129, 50})
                                     
-  progressTest2:setImages(love.graphics.newImage("progress_grey.png"),
-                         love.graphics.newImage("progress_green.png"))
+  progressTest2:setImages(love.graphics.newImage("images/progress_grey.png"),
+                         love.graphics.newImage("images/progress_green.png"))
                        
   progressTest2:setValue(0)
 
@@ -103,11 +103,11 @@ function love.update(dt)
 
   groupTest:update(dt)
   
-  if progressTest1.Value > 0.01 then
-    progressTest1:setValue(progressTest1.Value - 0.01)
+  if progressTest1.Value >= 0.1 then
+    progressTest1:setValue(progressTest1.Value - 0.1)
   end
-  if progressTest2.Value < 100 then
-    progressTest2:setValue(progressTest2.Value + 0.01)
+  if progressTest2.Value <= 100 then
+    progressTest2:setValue(progressTest2.Value + 0.1)
   end
 
 end
